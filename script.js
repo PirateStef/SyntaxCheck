491a3f999edd839138927cd4b14610776d79c55c
Stef Kors <stef.kors@gmail.com>
2016-01-21 15:47:53 +0100

    commit

---
+++ script.js
@@ -0,0 +1,31 @@
+function search() {
+  var search = $("input").val();
+  var url_search = "https://developer.mozilla.org/en-US/search.json?q=" + search;
+  $.getJSON(url_search, function(data){
+    console.log(url_search);
+    if ( $('.content').children().length >= 1) {
+      emptySearchContent();
+      loadSearchContent();
+    } else {
+      loadSearchContent();
+    }
+    function loadSearchContent() {
+      for (var i = 0; i < data.documents.length; i++) {
+        var a             = document.createElement("a");
+        var linkName      = document.createTextNode(data.documents[i].title);
+        var linkUrl       = data.documents[i].url;
+        var el            = document.createElement("div");
+        a.appendChild(linkName);
+        a.href            = linkUrl;
+        el.appendChild(a);
+        el.className      = "el";
+        $('.content').append(el);
+      };
+    };
+    function emptySearchContent() {
+      $('.content').children().remove();
+    };
+
+
+  });
+};
